0d142fc547f0982833108497b399712bbc5a7ce5
Reena Gupta <reena.gupta@walmartlabs.com>
2021-03-01 14:53:42 -0800

    initial commit

---
+++ client/src/components/sample.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { navigate, Link } from '@reach/router';
+import axios from 'axios';
+
+const AuthorAll = (props) => {
+  return <div>Author All</div>;
+};
+
+export default AuthorAll;
